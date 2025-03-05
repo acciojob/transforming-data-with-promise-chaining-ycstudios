228f49fc36efb4d5d4d7a0e849b5a8c04b74c9dc
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-03-05 19:22:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,23 +11,23 @@ btn.addEventListener("click", () => {
         }, 2000);
     })
         .then((ip) => {
-            output.innerHTML = `Result: ${ip}`; // Display initial value
+            output.innerHTML += `Result: ${ip} <br>`; // Append initial value
             return ip * 2; // Multiply by 2
         })
         .then((ip) => {
-            output.innerHTML = `Result: ${ip}`; // Display after multiplication
+            output.innerHTML += `Result: ${ip} <br>`; // Append after multiplication
             return ip - 3; // Subtract 3
         })
         .then((ip) => {
-            output.innerHTML = `Result: ${ip}`; // Display after subtraction
+            output.innerHTML += `Result: ${ip} <br>`; // Append after subtraction
             return ip / 2; // Divide by 2
         })
         .then((ip) => {
-            output.innerHTML = `Result: ${ip}`; // Display after division
+            output.innerHTML += `Result: ${ip} <br>`; // Append after division
             return ip + 10; // Add 10
         })
         .then((ip) => {
-            output.innerHTML = `Final Result: ${ip}`; // Display final result
+            output.innerHTML += `Final Result: ${ip} <br>`; // Append final result
         })
         .catch((error) => {
             output.innerHTML = `Error: ${error.message}`; // In case of an error
